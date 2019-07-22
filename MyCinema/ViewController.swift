3b1ad0903eb594c29b5c9a4227a154ce1b42a385
new_user_name <new@email.com>
2019-07-22 10:30:48 +0300

    Initial Commit

---
+++ MyCinema/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCinema
+//
+//  Created by Илья Маркелов on 22/07/2019.
+//  Copyright © 2019 Илья Маркелов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
