b60cc9bb0d828e8d5e72b790059e4a61f6130131
new_user_name <new@email.com>
2019-07-24 10:15:24 +0300

    Custom table view cell

---
+++ MyCinema/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyCinema
+//
+//  Created by Илья Маркелов on 22/07/2019.
+//  Copyright © 2019 Илья Маркелов. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+}
