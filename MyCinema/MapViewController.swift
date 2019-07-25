0b2ee881e2ca2c145935f42ace53b4365f251202
new_user_name <new@email.com>
2019-07-25 15:38:24 +0300

    Map View Controller

---
+++ MyCinema/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  MyCinema
+//
+//  Created by Илья Маркелов on 25/07/2019.
+//  Copyright © 2019 Илья Маркелов. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+}
