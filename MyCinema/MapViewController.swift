31729a0764512c83a3416b19340a8052d98630ad
new_user_name <new@email.com>
2019-07-25 16:16:25 +0300

    Work with annotations

--- MyCinema/MapViewController.swift
+++ MyCinema/MapViewController.swift
@@ -12,9 +12,11 @@ import MapKit
 class MapViewController: UIViewController {
 
     var cinema: Cinema!
+    let annotationIdentifier = "annotationIdentifier"
     
     @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
+        mapView.delegate = self
         super.viewDidLoad()
         setupPlacemark()
 
@@ -51,5 +53,29 @@ class MapViewController: UIViewController {
             self.mapView.selectAnnotation(annotation, animated: true)
         }
     }
-    
+}
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard !(annotation is MKUserLocation) else {return nil}
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) as? MKPinAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView?.canShowCallout = true
+        }
+        
+        if let imageData = cinema.imageData {
+            
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            imageView.layer.cornerRadius = 10
+            imageView.clipsToBounds = true
+            imageView.image = UIImage(data: imageData)
+            annotationView?.rightCalloutAccessoryView = imageView
+        }
+        
+        
+        return annotationView
+    }
 }
